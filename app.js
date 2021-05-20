d3646d8beba9e2d9e6f981fc4ff938afc94f0a83
“Italo <italo.queiroz@ykt.com.br>
2021-05-19 22:14:47 -0300

    Comentarios no arquivo app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var app = require('./config/server');
+var app = require('./config/server'); // a variável app é carregada
 
 
-var server = app.listen(80, function(){
+var server = app.listen(80, function(){ // agora a variavel app se conecta a uma das portas, nesse caso a 80
     console.log("Sistema online")
 })
 
